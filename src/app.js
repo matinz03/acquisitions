3be32acf276d6587a01c19767806322c7af4f321
Matin Zomorrodabedi <s307223@studenti.polito.it>
2025-09-17 10:26:13 +0200

    feat: add logger

--- src/app.js
+++ src/app.js
@@ -1,7 +1,22 @@
+import logger from '#config/logger.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
 const app = express();
-
+app.use(helmet());
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  morgan('combined', {
+    stream: { write: message => logger.info(message.trim()) },
+  })
+);
 app.get('/', (req, res) => {
+  logger.info('Hello from logger');
   res.status(200).send('hello from Acquisitions!');
 });
 export default app;
